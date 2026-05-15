460b88704e6c8627bbcda73c4d17a7c8d8c6d42c
J.Viper <viperjooy@gmail.com>
2026-05-15 17:33:17 +0800

    fix: MUI v9 改用 slotProps.input 替代 InputProps

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -40,34 +40,36 @@ function SearchBar({ value = '', onChange, onSearch, placeholder = '搜索壁纸
           pr: 0.5,
         },
       }}
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <SearchIcon sx={{ color: isDark ? 'rgba(255,255,255,0.85)' : 'text.secondary' }} />
-          </InputAdornment>
-        ),
-        endAdornment: (
-          <InputAdornment position="end">
-            {localValue && (
-              <IconButton size="small" onClick={handleClear} sx={{ mr: 0.5 }}>
-                <ClearIcon fontSize="small" />
+      slotProps={{
+        input: {
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon sx={{ color: isDark ? 'rgba(255,255,255,0.85)' : 'text.secondary' }} />
+            </InputAdornment>
+          ),
+          endAdornment: (
+            <InputAdornment position="end">
+              {localValue && (
+                <IconButton size="small" onClick={handleClear} sx={{ mr: 0.5 }}>
+                  <ClearIcon fontSize="small" />
+                </IconButton>
+              )}
+              <IconButton
+                size="small"
+                onClick={() => onSearch?.(localValue)}
+                sx={{
+                  bgcolor: 'primary.main',
+                  color: 'white',
+                  borderRadius: '6px',
+                  '&:hover': { bgcolor: 'primary.dark' },
+                  '& .MuiSvgIcon-root': { fontSize: 20 },
+                }}
+              >
+                <SearchIcon />
               </IconButton>
-            )}
-            <IconButton
-              size="small"
-              onClick={() => onSearch?.(localValue)}
-              sx={{
-                bgcolor: 'primary.main',
-                color: 'white',
-                borderRadius: '6px',
-                '&:hover': { bgcolor: 'primary.dark' },
-                '& .MuiSvgIcon-root': { fontSize: 20 },
-              }}
-            >
-              <SearchIcon />
-            </IconButton>
-          </InputAdornment>
-        ),
+            </InputAdornment>
+          ),
+        },
       }}
     />
   );
